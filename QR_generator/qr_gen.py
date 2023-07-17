5a85fdd40ac68ec185135a559f91af10687b3e82
Stefan Iancu <iancustefan28@yahoo.com>
2023-07-17 16:53:08 +0300

    fixed codes

--- QR_generator/qr_gen.py
+++ QR_generator/qr_gen.py
@@ -1,18 +1,34 @@
 import qrcode
 from PIL import Image
 
+CODE_PATH = "/Users/stefantraianiancu/Desktop/projects/QR_generator/codes"
+
 # set a desired website 
-data = "https://github.com/StefanIancu/projects"
+# data = "https://github.com/StefanIancu/projects"
 
 # generate qr
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
-qr.add_data(data)
-qr.make(fit=True)
+# qr.add_data(data)
+# qr.make(fit=True)
 
 # create an image from the qr
-image = qr.make_image(fill="black", back_color="white")
+# image = qr.make_image(fill="black", back_color="white")
 
 # save the image
-image.save("/Users/stefantraianiancu/Desktop/projects/QR_generator/qr_code.png")
-Image.open("/Users/stefantraianiancu/Desktop/projects/QR_generator/qr_code.png")
+# image.save("/Users/stefantraianiancu/Desktop/projects/QR_generator/qr_code.png")
+# Image.open("/Users/stefantraianiancu/Desktop/projects/QR_generator/qr_code.png")
 
+def make_qr():
+    while True:
+        link = input("Please enter your link: ")
+        if not link.startswith("www."):
+            print("Please use correct a correct format like -> www.link.com")
+        else:
+            qr.add_data(link)
+            qr.make(fit=True)
+            image = qr.make_image(fill = "black", back_color = "white")
+            image.save(f"{CODE_PATH}/{link.split('.')[1]}.png")
+            print("QR Code successfully generated.")
+            break
+
+make_qr()
--- QR_generator/readme.md
+++ QR_generator/readme.md
@@ -8,3 +8,17 @@
 
 <li>Helps the user generate a free QR Code for their website</li>
 <li>Unlike the other online solutions, this QR Code never expires</li>
+
+## First look 
+
+When the user runs the application, it asks for the link. If the link doesn't have the correct format, the user cannot proceed further. 
+
+*the input doesn't verify the termination of the domain link (".com", ".org", ".net"), it just verifies that the string starts with "www."
+
+<img width="456" alt="Screenshot 2023-07-17 at 16 43 54" src="https://github.com/StefanIancu/projects/assets/124818078/a20fe665-9157-447e-8332-eea73b2768a7">
+
+If the link respects the format, the QR code will be generated and saved into "codes" directory. The name of the .png file would be the name of the website. 
+
+<img width="450" alt="Screenshot 2023-07-17 at 16 45 13" src="https://github.com/StefanIancu/projects/assets/124818078/c4b5eedb-ad37-4aad-bbff-63e725955462">
+
+<img width="234" alt="Screenshot 2023-07-17 at 16 45 39" src="https://github.com/StefanIancu/projects/assets/124818078/a99b0613-425a-4ee2-bd4e-ca73cdb1fd20">
