7fa1f3a92052d1b5bd7a87f897d9d73eb848d496
Stefan Iancu <iancustefan28@yahoo.com>
2023-07-20 14:57:09 +0300

    implemented gui interface for converter

--- currency_converter/currency_converter.py
+++ currency_converter/currency_converter.py
@@ -1,7 +1,29 @@
 import tkinter as tk
 from forex_python.converter import CurrencyRates
+from datetime import datetime
+import geocoder
 
 c = CurrencyRates()
+
+now = datetime.now()
+formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+my_location = geocoder.ip("me")
+location = str(my_location[::-1]).strip("[]")
+
+
+def convert_to_eur(ron:float):
+    to_eur = c.convert("RON", "EUR", ron)
+    to_usd = c.convert("RON", "USD", ron)
+    to_gbp = c.convert("RON", "GBP", ron)
+    return [round(to_eur, 2), round(to_usd, 2), round(to_gbp, 2)]
+
+def process_command(input_cmp, output_cmp, outcmp2, outcmp3, f):
+    input_value = float(input_cmp.get())
+    output_value = f(input_value)
+    output_cmp["text"] = f"{output_value[0]} EUR"
+    outcmp2["text"] = f"{output_value[1]} USD"
+    outcmp3["text"] = f"{output_value[2]} GBP"
 
 
 window = tk.Tk()
@@ -13,6 +35,11 @@ frame = tk.Frame(master=window)
 entry_currency = tk.Entry(master=frame, width=20)
 entry_currency.grid(row=2, column=0, sticky="e")
 
+date_today = tk.Label(master=frame, text=f"{formatted_time}")
+date_today.grid(row=1, column=0, sticky="w")
+
+user_location = tk.Label(master=frame, text=f"{location}")
+user_location.grid(row=3, column=0, sticky="w")
 
 label_currency = tk.Label(master=frame, text="EUR")
 label_currency.grid(row=1, column=3, sticky="w")
@@ -23,18 +50,16 @@ label_currency2.grid(row=2, column=3, sticky="w")
 label_currency3 = tk.Label(master=frame, text="GBP")
 label_currency3.grid(row=3, column=3, sticky="w")
 
-# label_result = tk.Label(master=frame, text="RON")
-# label_result.grid(row=3, column=1, padx=10)
-
 frame.grid(row=1, column=1, padx=10)
+frame.grid(row = 0, column= 0)
 
 button = tk.Button(
     master=frame,
-    text="RON\N{RIGHTWARDS BLACK ARROW}"
+    text="RON\N{RIGHTWARDS BLACK ARROW}",
+    command= lambda : process_command(entry_currency, label_currency, label_currency2, label_currency3, convert_to_eur),
 )
 button.grid(row=2, column=1, pady=10)
 
-frame.grid(row = 0, column= 0)
 
 
 window.mainloop()
---
+++ currency_converter/readme.md
@@ -0,0 +1,25 @@
+### Currency Converter using ``forex_python's`` rates
+
+## Overlook 
+
+<img width="333" alt="Screenshot 2023-07-20 at 14 52 08" src="https://github.com/StefanIancu/projects/assets/124818078/4c52fef6-5c7b-4dfc-87b3-ec298842b621">
+
+## Purpose
+
+Let's the user easily convert from RON up to other three currencies. 
+
+## First touch
+
+After the user opens the application, they'll need to introduce a value and click on 
+the "RON->" button. The value will be converted in the same time into the three currencies.
+
+<img width="366" alt="Screenshot 2023-07-20 at 14 52 29" src="https://github.com/StefanIancu/projects/assets/124818078/04ffcba4-2111-46ab-8341-73c01fdcfcad">
+
+
+## Location-based
+
+The app is telling the user his current location and the real date-time. For this example I turned off my VPN. 
+
+<img width="363" alt="Screenshot 2023-07-20 at 14 54 45" src="https://github.com/StefanIancu/projects/assets/124818078/f671aa6a-dd91-499d-939f-0ba83e7fd9b5">
+
+
