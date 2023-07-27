1924b62b833a393298302513f0dd18af7c7aa118
Stefan Iancu <iancustefan28@yahoo.com>
2023-07-27 11:53:37 +0300

    implemented sync solution

--- synchronization_program/sync.py
+++ synchronization_program/sync.py
@@ -8,6 +8,7 @@ import logging
 import shutil
 import os
 import time
+from datetime import datetime
 
 # setting the paths
 ROOT = Path(__file__).parent
@@ -19,28 +20,31 @@ logging.basicConfig(filename=f"{ROOT}/log.log", level=logging.DEBUG,
                      format="%(asctime)s [%(levelname)s] %(message)s")
 
 def delete(msg):
-    """Prints the logger info on the console."""
+    """Prints a message on the console. Works parallel with the logger."""
     print(f"[INFO] {msg} deleted.")
 
 def show(msg):
+    """Prints a message on the console. Works parallel with the logger."""
     print(f"[INFO] {msg}")
+
+def logging_source(source):
+    """Logs the location where the program is running."""
+    logging.info(f"Working in {source}.")
+
+# logs each folder that has been copied and created
+def copy2_verbose(src, repl):
+    for path in os.listdir(src):
+        if path not in os.listdir(repl):
+            logging.info(f"{path} was created and copied to replica.")
+            show(f"{path} was created and copied.")
 
 # copies the content of the source folder to the replica folder
 def copy_source_to_replica(source, replica):
     """Copies the content of source to replica."""
     try:
-        shutil.copytree(source, replica, dirs_exist_ok=True)
+        shutil.copytree(source, replica, dirs_exist_ok=True, ignore=copy2_verbose(source, replica))
     except ValueError as err:
         print(err)
-
-def check_new_file(source, replica):
-    """Checks if there are any new files created."""
-    _list = lambda list : len(list)
-    initial_list = _list(os.listdir(source))
-    source_list = os.listdir(source)
-    if len(source_list) > initial_list:
-        print("A folder was created")
-
 
 # checks both dirs and deletes what is not a match. logs the details and 
 # prints on the console as well
@@ -62,8 +66,8 @@ def check_file_integrity(source, replica):
 
 # runs the program in an infinite loop
 def running_program(source, replica):
+    logging_source(source)
     while True:
-        check_new_file(source, replica)
         time.sleep(2)
         copy_source_to_replica(source, replica)
         print("Content synced.")
